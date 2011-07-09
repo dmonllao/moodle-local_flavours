b837ac7fe193abce22461a7aa4611d9de8789fc5
David Monllaó <david.monllao@gmail.com>
2011-07-09 20:52:40 +0200

    MINOR - CONTRIB-2959

--- forms/flavours_deployment_upload_form.php
+++ forms/flavours_deployment_upload_form.php
@@ -24,7 +24,7 @@ class flavours_deployment_upload_form extends moodleform {
         $overwriteoptions = array(0 => get_string('overwriteno', 'local_flavours'),
             1 => get_string('overwriteyes', 'local_flavours'));
         $mform->addElement('select', 'overwrite', $overwritelabel, $overwriteoptions);
-        $mform->setType('overwrite', PARAM_ALPHA);
+        $mform->setType('overwrite', PARAM_INT);
 
         $mform->addElement('hidden', 'action', 'deployment_preview'); 
         $mform->addElement('submit', 'deployment_upload_submit', get_string('next'));

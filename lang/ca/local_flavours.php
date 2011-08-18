1927f50f707e0f63fde075283b774bc162f30002
David Monllaó <david.monllao@gmail.com>
2011-08-18 11:53:34 +0200

    CONTRIB-2848 - Language translations to Catalan and Spanish

---
+++ lang/ca/local_flavours.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Catalan language strings
+ *
+ * @package    local
+ * @subpackage flavours
+ * @copyright  2011 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['actionnotsupported'] = 'Acció no suportada en aquesta versió';
+$string['actiondeployment_execute'] = 'Desplegament de sabors';
+$string['actiondeployment_preview'] = 'Desplegament de sabors - Previsualizació dels ingredients';
+$string['actiondeployment_upload'] = 'Desplegament de sabors - Pujar un sabor';
+$string['actionpackaging_form'] = 'Empaquetament de sabors';
+$string['actionpackaging_execute'] = 'Empaquetament de sabors';
+$string['deploy'] = 'Desplega un sabor';
+$string['deploymentcontinue'] = 'Continua';
+$string['deploymentpreviewheader'] = 'Pas 2 - Previsualizació d\'ingredients';
+$string['deploymentresult'] = 'Resultats del desplegament';
+$string['deploymentuploadheader'] = 'Pas 1 - Pujar un sabor';
+$string['errorcantunzip'] = 'El sabor no es pot descomprimir';
+$string['errorcopying'] = 'Error copiant ingredients';
+$string['errordeployflavour'] = 'Problema obtenint l\'arxiu del sabor';
+$string['errordeployingpermissions'] = 'Comprova els permissos de la carpeta temporal';
+$string['errordeploying'] = 'Error desplegant el sabor';
+$string['errorpackaging'] = 'Error empaquetant el sabor';
+$string['errorunknownsettingspage'] = 'Pàgina de paràmetres de configuració desconeguda';
+$string['deployflavour'] = 'Deplega';
+$string['flavourauthor'] = 'Autor';
+$string['flavourdata'] = 'Informació del sabor';
+$string['flavourdescription'] = 'Descripció';
+$string['flavoursourceurl'] = 'Lloc d\'origen';
+$string['flavoursourcemoodleversion'] = 'Versió de Moodle d\'origen';
+$string['flavoursourcemoodlerelease'] = 'Moodle d\'origen';
+$string['flavourname'] = 'Nom';
+$string['flavourtimecreated'] = 'Creat el';
+$string['ingredient'] = 'Ingredient';
+$string['ingredienttype'] = 'Tipus d\'ingredient';
+$string['ingredienttypenotavailable'] = 'Tipus d\'ingredient no disponible';
+$string['nothingselected'] = 'Has de seleccionar alguna cosa';
+$string['overwrite'] = 'Sobreescriure';
+$string['overwriteno'] = 'No sobreescriure plugins ja instal·lats';
+$string['overwriteyes'] = 'Només sobreescriure plugins amb versions superiors al sabor';
+$string['package'] = 'Empaqueta un sabor';
+$string['packageflavour'] = 'Empaqueta';
+$string['pluginname'] = 'Sabors';
+$string['problem'] = 'Problema!';
+$string['reselect'] = 'No hi ha res seleccionat!';
+$string['restrictioncustomlangnotinstalled'] = 'El paquet d\'idioma "{$a}" no està instal·lat al sistema';
+$string['restrictioncustomlangalreadycreated'] = 'Ja disposes d\'una modificació del paquet d\'idioma';
+$string['restrictionlangalreadyinstalled'] = 'Paquete d\'idioma "{$a}" ja disponible';
+$string['restrictionlangfilepermissions'] = 'Permissos incorrectes';
+$string['restrictionlangnotvalid'] = 'Paquet d\'idioma "{$a}" no vàlid';
+$string['restrictionpluginalreadyinstalled'] = 'Plugin "{$a}" ja instal·lat';
+$string['restrictionplugincantremove'] = 'La versió actual de "{$a}" no es pot actualizar, revisa els permissos';
+$string['restrictionplugincopyerror'] = 'Error copiant';
+$string['restrictionpluginflavournotnewer'] = 'La versió del sabor no és més nova que la instal·lada actualment';
+$string['restrictionpluginnowritable'] = 'Permissos incorrectes per a instal·lar o actualitzar a "{$a}"';
+$string['restrictionpluginnotfound'] = 'Plugin "{$a}" no trobat';
+$string['restrictionpluginnoversiondiskupgrade'] = 'El plugin "{$a}" j existeix i no es pot actualitzar, el plugin del sabor no indica la seva versió';
+$string['restrictionpluginsystemold'] = 'El plugin requereix una versió superior de Moodle';
+$string['restrictionsettingnosettingpage'] = 'La pàgina de paràmetres de configuració "{$a}" no existeix';
+$string['restrictionsettingsproblemsarray'] = 'Problemas desplegant aquests paràmetres: "{$a}"';
+$string['restrictionsettingwarningoverwrite'] = 'Els paràmetres actuals seran sobreescrits';
+$string['selectfile'] = 'Selecciona arxiu';
+$string['selectingredients'] = 'Selecciona ingredients';
+$string['selectingredientstodeploy'] = 'Selecciona els ingredients a desplegar';
+$string['warning'] = 'Atenció!';
---
+++ lang/es/local_flavours.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Español language strings
+ *
+ * @package    local
+ * @subpackage flavours
+ * @copyright  2011 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['actionnotsupported'] = 'Acción no soportada en esta versión';
+$string['actiondeployment_execute'] = 'Despliegue de sabores';
+$string['actiondeployment_preview'] = 'Despliegue de sabores - Previsualización de los ingredientes';
+$string['actiondeployment_upload'] = 'Despliegue de sabores - Subir un sabor';
+$string['actionpackaging_form'] = 'Empaquetado de sabores';
+$string['actionpackaging_execute'] = 'Empaquetado de sabores';
+$string['deploy'] = 'Despliega un sabor';
+$string['deploymentcontinue'] = 'Continua';
+$string['deploymentpreviewheader'] = 'Paso 2 - Previsualización de ingredientes';
+$string['deploymentresult'] = 'Resultados del despliegue';
+$string['deploymentuploadheader'] = 'Paso 1 - Subir un sabor';
+$string['errorcantunzip'] = 'El sabor no se puede descomprimir';
+$string['errorcopying'] = 'Error copiando ingredientes';
+$string['errordeployflavour'] = 'Problema obteniendo el archivo del sabor';
+$string['errordeployingpermissions'] = 'Comprueba los permisos de la carpeta temporal';
+$string['errordeploying'] = 'Error desplegando el sabor';
+$string['errorpackaging'] = 'Error empaquetando el sabor';
+$string['errorunknownsettingspage'] = 'Página de parámetros de configuración desconocida';
+$string['deployflavour'] = 'Despliega';
+$string['flavourauthor'] = 'Autor';
+$string['flavourdata'] = 'Información del sabor';
+$string['flavourdescription'] = 'Descripción';
+$string['flavoursourceurl'] = 'Sitio de origen';
+$string['flavoursourcemoodleversion'] = 'Versión de Moodle de origen';
+$string['flavoursourcemoodlerelease'] = 'Moodle de origen';
+$string['flavourname'] = 'Nombre';
+$string['flavourtimecreated'] = 'Creado el';
+$string['ingredient'] = 'Ingrediente';
+$string['ingredienttype'] = 'Tipo de ingrediente';
+$string['ingredienttypenotavailable'] = 'Tipo de ingrediente no disponible';
+$string['nothingselected'] = 'Debes seleccionar algo';
+$string['overwrite'] = 'Sobreescribir';
+$string['overwriteno'] = 'No sobreescribir plugins ya instalados';
+$string['overwriteyes'] = 'Solo sobreescribir plugins con versiones superiores en el sabor';
+$string['package'] = 'Empaqueta un sabor';
+$string['packageflavour'] = 'Empaqueta';
+$string['pluginname'] = 'Sabores';
+$string['problem'] = 'Problema!';
+$string['reselect'] = 'No hay nada seleccionado!';
+$string['restrictioncustomlangnotinstalled'] = 'El paquete de idioma "{$a}" no está instalado en el sistema';
+$string['restrictioncustomlangalreadycreated'] = 'Ya dispones de una modificación del paquete de idioma';
+$string['restrictionlangalreadyinstalled'] = 'Paquete de idioma "{$a}" ya disponible';
+$string['restrictionlangfilepermissions'] = 'Permisos incorrectos';
+$string['restrictionlangnotvalid'] = 'Paquete de idioma "{$a}" no válido';
+$string['restrictionpluginalreadyinstalled'] = 'Plugin "{$a}" ya instalado';
+$string['restrictionplugincantremove'] = 'La versión actual de "{$a}" no se puede actualizar, revisa los permisos';
+$string['restrictionplugincopyerror'] = 'Error copiando';
+$string['restrictionpluginflavournotnewer'] = 'La versión del sabor no és más nueva que la instalada actualmente';
+$string['restrictionpluginnowritable'] = 'Permisos incorrectos para instalar o actualizar en "{$a}"';
+$string['restrictionpluginnotfound'] = 'Plugin "{$a}" no encontrado';
+$string['restrictionpluginnoversiondiskupgrade'] = 'El plugin "{$a}" ya existe y no se puede actualizar, el plugin del sabor no indica su versión';
+$string['restrictionpluginsystemold'] = 'El plugin requiere una versión superior de Moodle';
+$string['restrictionsettingnosettingpage'] = 'La página de parámetros de configuración "{$a}" no existe';
+$string['restrictionsettingsproblemsarray'] = 'Problemas desplegando estos parámetros: "{$a}"';
+$string['restrictionsettingwarningoverwrite'] = 'Los parámetros actuales serán sobreescritos';
+$string['selectfile'] = 'Selecciona archivo';
+$string['selectingredients'] = 'Selecciona ingredientes';
+$string['selectingredientstodeploy'] = 'Selecciona los ingredientes a desplegar';
+$string['warning'] = 'Atención!';
